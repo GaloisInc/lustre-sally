30331ff2a1683c426806a72c4109e1a0b7549195
Iavor Diatchki <iavor.diatchki@gmail.com>
2018-11-20 10:48:08 -0800

    Change trace format for UI (for easier display); implement showing steps

--- src/Report.hs
+++ src/Report.hs
@@ -1,10 +1,14 @@
+{-# Language OverloadedStrings #-}
 module Report where
 
-import Data.Foldable(toList)
+import Data.Map(Map)
+import qualified Data.Map as Map
+import qualified Text.PrettyPrint as PP
 
 import Language.Lustre.AST
 import Language.Lustre.ModelState
 import Language.Lustre.Pretty
+
 
 import Lustre(LTrace)
 import TransitionSystem(Trace(..))
@@ -12,11 +16,11 @@ import LSPanic(panic)
 
 
 declareSource :: String -> String
-declareSource s = "var source = " ++ js
+declareSource s = show ("var source =" PP.$$ PP.nest 2 js)
   where JS js = text s
 
 declareTrace :: ModelInfo -> LTrace -> String
-declareTrace mi tr = "var trace = " ++ js
+declareTrace mi tr = show ("var trace =" PP.$$ PP.nest 2 js)
   where JS js = case computeCallTree mi of
                   Just ct -> renderTrace ct tr
                   Nothing -> panic "declareTrace"
@@ -46,67 +50,52 @@ callTreeLoc l = Fun l [ (c,callTreeLoc l1)
 
 
 renderTrace :: CallTree -> LTrace -> JSON
-renderTrace ct tr = array (map step states)
+renderTrace ct tr = renderJSMap (foldr step Map.empty (zip [0..] states))
   where
-  step   = array . renderCallTree Nothing ct
-  states = traceStart tr : map snd (traceSteps tr)
+  step (i,stp) = renderCallTree i Nothing ct stp
+  states       = traceStart tr : map snd (traceSteps tr)
 
-renderCallTree :: Maybe CallSiteId -> CallTree -> S -> [JSON]
-renderCallTree mbcid (Fun l cs) s = start ++ concatMap next cs
+renderCallTree :: Int -> Maybe CallSiteId -> CallTree -> S -> JsMap -> JsMap
+renderCallTree stp mbcid (Fun l cs) s mp = foldr next start cs
   where
   start =
     case mbcid of
-      Just cid -> renderCS cid l s
-      Nothing  -> renderLoc "cs_top" l s
+      Just cid -> renderCS stp cid l s mp
+      Nothing  -> renderLoc (stp,"cs_top") l s mp
 
-  next (c,t) = renderCallTree (Just c) t s
+  next (c,t) = renderCallTree stp (Just c) t s
 
 
-renderCS :: CallSiteId -> Loc -> S -> [JSON]
-renderCS cid l s = this : renderLoc nm l s
+renderCS :: Int -> CallSiteId -> Loc -> S -> JsMap -> JsMap
+renderCS step cid l s = renderLoc (step,nm) l s . addJS cid this
   where
-  this = obj [ "line"   ~> int (sourceLine from)
-             , "from"   ~> int (sourceColumn from)
-             , "to"     ~> int (sourceColumn to)
-             , "cid"    ~> text nm
+  this = obj [ "cid"    ~> text nm
              , "source" ~> obj [ "from" ~> int (sourceLine (sourceFrom src))
                                , "to"   ~> int (sourceLine (sourceTo src))
                                ]
              ]
   nm   = callSiteName cid
-
-  loc  = range cid
-  from = sourceFrom loc
-  to   = sourceTo loc
-
   src  = range l
 
 
 
 
 -- | Render the variables for this calls site.
-renderLoc :: String {- ^ Call site identifier -} ->
-             Loc    {- ^ Funciton call to render -} ->
-             S      {- ^ Current state -} ->
-             [JSON]
+renderLoc :: (Int,String) {- ^ Step and call site identifier -} ->
+             Loc          {- ^ Funciton call to render -} ->
+             S            {- ^ Current state -} ->
+             JsMap -> JsMap
 renderLoc cid l s = renderVars cid (lookupVars l s)
 
-renderVars :: String -> Vars (SourceIdent, Maybe SourceValue) -> [JSON]
-renderVars cid = map (renderVar cid) . toList
+renderVars :: (Int,String) -> Vars (SourceIdent, Maybe SourceValue) ->
+              JsMap -> JsMap
+renderVars cid vs mp = foldr (renderVar cid) mp vs
 
-renderVar :: String -> (SourceIdent, Maybe SourceValue) -> JSON
-renderVar cid (i, mbV) =
-  obj [ "line"  ~> int (sourceLine from)
-      , "from"  ~> int (sourceColumn from)
-      , "to"    ~> int (sourceColumn to)
-      , "value" ~> renderMaybeValue mbV
-      , "cid"   ~> text cid
-      ]
-  where
-  loc  = range i
-  from = sourceFrom loc
-  to   = sourceTo loc
-
+renderVar :: (Int,String) -> (SourceIdent, Maybe SourceValue) -> JsMap -> JsMap
+renderVar (s,cid) (i, mbV) =
+  addJS i $ obj [ "value" ~> renderMaybeValue mbV , "cid"   ~> text cid
+                , "step"  ~> int s
+                ]
 
 renderValue :: SourceValue -> JSON
 renderValue v = text (showPP v)
@@ -117,33 +106,54 @@ renderMaybeValue mb =
     Nothing -> text "?"
     Just v  -> renderValue v
 
+-- | Maps: line -> (from-col,to-col) -> [json (e.g., value)]
+type JsMap = Map Int (Map (Int,Int) [JSON])
+
+addJS :: HasRange i => i -> JSON -> JsMap -> JsMap
+addJS i js = Map.insertWith (Map.unionWith (++)) (sourceLine from)
+           $ Map.singleton (sourceColumn from, sourceColumn to) [js]
+  where
+  loc  = range i
+  from = sourceFrom loc
+  to   = sourceTo loc
+
+renderJSMap :: JsMap -> JSON
+renderJSMap = obj . map renderLn . Map.toList
+  where
+  renderLn (l,xs)   = show l ~> renderAttrs xs
+  renderAttrs       = array . map renderAttr . Map.toList
+  renderAttr ((f,t),y)  = obj [ "from" ~> int f
+                              , "to" ~> int t
+                              , "attr" ~> array y
+                              ]
+
 
 --------------------------------------------------------------------------------
 
-newtype JSON = JS { unJS :: String }
+newtype JSON = JS { unJS :: PP.Doc }
 
 text :: String -> JSON
-text = JS . show
+text = JS . PP.text . show
 
 int :: Int -> JSON
-int = JS . show
+int = JS . PP.int
 
 array :: [JSON] -> JSON
 array = JS . block "[" "," "]" . map unJS
 
 obj :: [(String,JSON)] -> JSON
 obj = JS . block "{" "," "}" . map entry
-  where entry (a,b) = unJS (text a) ++ ": " ++ unJS b
+  where entry (a,b) = unJS (text a) PP.<> ":" PP.<+> unJS b
 
 (~>) :: String -> JSON -> (String,JSON)
 a ~> b = (a,b)
 
-block :: String -> String -> String -> [String] -> String
+block :: PP.Doc -> PP.Doc -> PP.Doc -> [PP.Doc] -> PP.Doc
 block open sep close xs =
   case xs of
-    [] -> open ++ close
-    a : as -> unlines $ (open ++ " " ++ a)
-                      : [ sep ++ " " ++ b | b <- as ] ++
+    [] -> open PP.<> close
+    a : as -> PP.vcat $ (open PP.<+> a)
+                      : [ sep PP.<+> b | b <- as ] ++
                         [ close ]
 
 
--- ui/ui.js
+++ ui/ui.js
@@ -1,67 +1,113 @@
-function annotationName(line,from,to) {
-  return 'ann_' + line + '_' + from + '_' + to
-}
+function moduleViewTrace () {
+
+  // The state
+  var curStep = 0
+  var callStack = [ "cs_top" ]
+  var maxStep = 0
+
+  // Names
+  function stepName(i) { return 'step-' + i }
+  var stepDisplayId = 'step-display'
 
 
+  // Show steps
+  function showStep(s) {
+    if (s > maxStep) return
+    if (s < 0) return
 
-function drawSource(source, trace) {
-  // source      : String
-  // annotations : [Annotation]
-  // Annotation  = { line, from, to : Int, value: Showable }
-  // Assumption: annotations do not overlap
-
-  var dom = $('<div/>').addClass('code')
-
-  var annMap = {}
-  function getAnn(line) {
-    var thisLine = annMap[line]
-    return thisLine === undefined? [] : thisLine
+    $('.' + stepName(curStep)).hide()
+    $('.' + stepName(s)).show()
+    $('#' + stepDisplayId).text(s)
+    curStep = s
+    displayCurStep()
   }
 
-  // Group annotations by line
-  jQuery.each(trace, function(step,tr) {
-    jQuery.each(tr, function(ix,ann) {
-      var thisLine = getAnn(ann.line)
-      var newAnn = jQuery.extend({step:step},ann)
-      thisLine.push(newAnn)
-      annMap[ann.line] = thisLine
+  function nextStep() { showStep(curStep+1) }
+  function prevStep() { showStep(curStep-1) }
+
+  function displayCurStep() {
+    $('#' + stepDisplayId).text(curStep === 0 ? 'initializing' : curStep)
+  }
+
+  function stepButtons() {
+    var prev = $('<button/>')
+               .addClass('step-button')
+               .text('<<')
+               .click(prevStep)
+    var next = $('<button/>')
+               .addClass('step-button')
+               .text('>>')
+               .click(nextStep)
+    var label = $('<span/>').text('step:')
+    var display = $('<span/>').attr('id',stepDisplayId)
+    return [prev,label,display,next]
+  }
+
+
+
+  // Drawing
+
+  function drawSource(source, attrs) {
+    // source : String
+    // attrs : { Line ~> [ {from,to: Int, attr: [Attr]} ] }
+    // Line = String
+    // Attr = { step: Int, cid: String, value: String }
+
+    var dom = $('<div/>').addClass('code')
+
+    function getAnn(ln) {
+      var res = attrs[ln.toString()]
+      return (res === undefined) ? [] : res
+    }
+
+    // Render the lines
+    var lines = source.split('\n')
+    jQuery.each(lines,function(lineNoPrev,txt) {
+      var ln = $('<div/>').addClass('line')
+      var start = 0
+      jQuery.each(getAnn(lineNoPrev+1),function(ix,ann) {
+        var from = ann.from - 1   // columns start at 1
+        if (from > start) {
+          ln.append($('<span/>').text(txt.substring(start,from)))
+        }
+        start = ann.to
+        var annDom = $('<span/>')
+                     .addClass('annot')
+                     .text(txt.substring(from,start))
+        ln.append(annDom)
+
+        jQuery.each(ann.attr,function(ix,attr) {
+          if (attr.value === undefined) {
+          } else {
+             var val = $('<span/>')
+                     .addClass('value')
+                     .addClass(attr.cid)
+                     .addClass(stepName(attr.step))
+                     .text(attr.value)
+                     .hide()
+             if (attr.step > maxStep) maxStep = attr.step
+             var eq = $('<span/>')
+                    .text('=')
+                    .addClass(attr.cid)
+                    .addClass(stepName(attr.step))
+                    .hide()
+             ln.append(eq,val)
+          }
+        })
+
+      })
+      ln.append($('<span/>').text(txt.substring(start)))
+
+      dom.append(ln)
     })
-  })
 
-  // Render the lines
-  var lines = source.split('\n')
-  jQuery.each(lines,function(lineNoPrev,txt) {
-    var lineNo = lineNoPrev + 1
-    var ln = $('<div/>').addClass('line')
-    var sorted = getAnn(lineNo).sort(function(a,b) { return a.from - b.from })
-    var start = 0
-    jQuery.each(sorted,function(ix,ann) {
-      var from = ann.from - 1   // columns start at 1
-      if (from > start ) {
-        ln.append($('<span/>').text(txt.substring(start,from)))
-      }
-      start = ann.to
-      var annDom = $('<span/>')
-                   .addClass('annot')
-                   .text(txt.substring(from,start))
+    return dom
+  }
 
-      var valAnn = ann.value
-      if (valAnn === undefined) {
-        // XXX
-      } else {
-        var val = $('<span/>')
-                  .addClass('value')
-                  .addClass(ann.cid)
-                  .addClass('step-' + ann.step)
-                  .text(ann.value)
-        ln.append(annDom,val)
-      }
-    })
-    ln.append($('<span/>').text(txt.substring(start)))
+  // The public interface
+  return { drawSource: drawSource
+         , stepButtons: stepButtons
+         , showStep: showStep
+         }
 
-    dom.append(ln)
-  })
-
-  return dom
 }
-
